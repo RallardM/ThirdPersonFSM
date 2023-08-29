3a609c92805376d485ef915ce8f3656a9e4ea98a
Remi Allard Mayer <remallmay@outlook.com>
2023-08-29 13:46:13 -0400

    Tried new light settings

--- Assets/Resources/Scripts/CameraController.cs
+++ Assets/Resources/Scripts/CameraController.cs
@@ -1,4 +1,6 @@
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class CameraController : MonoBehaviour
 {
@@ -8,6 +10,17 @@ public class CameraController : MonoBehaviour
     private float m_rotationSpeed = 1.0f;
     [SerializeField]
     private Vector2 m_clampingXRotationValues = Vector2.zero;
+
+    private Vector3 m_farthestCamDist = new(0, 74, 300);
+    private Vector3 m_farthestCamRotation = new(10, 0, 0);
+
+    private Vector3 m_closestCamDist = new(0, 1, 13);
+    private Vector3 m_closestCamRotation = new(0, 0, 0);
+
+    private float m_scrollSpeed = 2.0f;
+
+    float m_farthestCamDistFOV = 3.5f;
+    float m_closestCamDistFOV = 24f;
 
     private void Awake()
     {
@@ -46,14 +59,34 @@ public class CameraController : MonoBehaviour
 
     private void UpdateCameraScroll()
     {
-        if (Input.mouseScrollDelta.y != 0)
+        if (Input.mouseScrollDelta.y == 0)
         {
-            //TODO: Faire une vérification selon la distance la plus proche ou la plus éloignée
-            //Que je souhaite entre ma caméra et mon objet
-
-            //TODO: Lerp plutôt que d'effectuer immédiatement la translation
-            transform.Translate(Vector3.forward * Input.mouseScrollDelta.y, Space.Self);
+            return;
         }
+
+        //TODO: Faire une vérification selon la distance la plus proche ou la plus éloignée
+        //Que je souhaite entre ma caméra et mon objet
+
+        Vector3 camTranslation = new(0, 0, 0);
+
+        float currentCamDist = GetDistBetweenTwoVects(transform.position, m_objectToLookAt.position);
+        float farthestCamDist = GetDistBetweenTwoVects(transform.position + m_farthestCamDist, m_objectToLookAt.position);
+        float closestCamDist = GetDistBetweenTwoVects(transform.position + m_closestCamDist, m_objectToLookAt.position);
+
+        if (currentCamDist > farthestCamDist || currentCamDist < closestCamDist)
+        {
+            return;
+        }
+        else
+        {
+            camTranslation = Vector3.forward;
+        }
+
+        //TODO: Lerp plutôt que d'effectuer immédiatement la translation
+        //Vector3 scrollPosLerp = Vector3.Lerp(transform.position, transform.position + camTranslation, Time.deltaTime);
+
+        transform.Translate(camTranslation * Input.mouseScrollDelta.y * m_scrollSpeed, Space.Self);
+        //transform.Translate(scrollPosLerp, Space.Self);
     }
 
     private float ClampAngle(float angle)
@@ -63,5 +96,10 @@ public class CameraController : MonoBehaviour
             angle -= 360;
         }
         return angle;
+    }
+
+    private float GetDistBetweenTwoVects(Vector3 firstVector, Vector3 secondVector)
+    {
+        return Vector3.Distance(firstVector, secondVector);
     }
 }
