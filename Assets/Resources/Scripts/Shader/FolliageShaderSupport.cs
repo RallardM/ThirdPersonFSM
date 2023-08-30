9960c44eeae383e947319597d4f9ae736618a826
Remi Allard Mayer <remallmay@outlook.com>
2023-08-30 01:48:20 -0400

    Added shader graph to correct bark and foliage

---
+++ Assets/Resources/Scripts/Shader/FolliageShaderSupport.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:5ee529df35f557899bdc1a53291f6dd80f92091ecd0a56ff8613ba8c4983e56e
+size 4382
---
+++ Assets/Resources/ShaderGraphs/FoliageShader/FoliageLighting.hlsl
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:cd1be9a35ef9d3940a5cf03f207937283b61c9a5031d7c256f8e1d7c2f71b10b
+size 8172
