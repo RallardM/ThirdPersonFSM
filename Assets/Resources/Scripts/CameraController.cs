5169d0f0d72dea48e2e5ec42317e6b6aed7d4764
Remi Allard Mayer <remallmay@outlook.com>
2023-09-06 12:31:33 -0400

    follow cam better in lateupdate

--- Assets/Resources/Scripts/CameraController.cs
+++ Assets/Resources/Scripts/CameraController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f02e9206c1ca135c76e9dec59a5590b775a722a542a2b412c5531e8be11acf97
-size 4599
+oid sha256:8a175db0976721f18a80764421d2bcd049f6936caea1f840e4a449c760036d14
+size 4557
