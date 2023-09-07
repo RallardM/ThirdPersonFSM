5c27c4a6d3e49ad6a920e5c2fb12972ef21c4274
Remi Allard Mayer <remallmay@outlook.com>
2023-09-07 11:19:42 -0400

    Todo done

--- Assets/Resources/Scripts/CharacterControllerStateMachine.cs
+++ Assets/Resources/Scripts/CharacterControllerStateMachine.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0d70c64941e06ec31db5f332e6af5753c6aceb2dbb1751af790047b8510e47d1
-size 1147
+oid sha256:f850d30ffe7cbe1715a64920a41d5e78f4b2484358d9d80eb6c977385c9b3ed3
+size 1196
