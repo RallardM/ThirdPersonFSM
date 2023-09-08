0e2d2e62ce2b2e6c2e00a35fe122ea7bfd4ba67e
Remi Allard Mayer <remallmay@outlook.com>
2023-09-08 16:21:57 -0400

    better scrolling smooth

--- Assets/Resources/Scripts/CameraController.cs
+++ Assets/Resources/Scripts/CameraController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0ecc18b480464777b7114986f3b8680686460e87b69418207110a08917f9dca2
-size 6424
+oid sha256:c9efad4782e0379b75dcd7c5a3c5d2faf5c654307c9df439dd6feee5b185c043
+size 6516
