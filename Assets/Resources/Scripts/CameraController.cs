2b69a3d3ab9edceea199a416d91d132712176d72
Remi Allard Mayer <remallmay@outlook.com>
2023-09-08 16:56:05 -0400

    remove debug log

--- Assets/Resources/Scripts/CameraController.cs
+++ Assets/Resources/Scripts/CameraController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f2568ae8572f8875e883aaae3d51b35ca3a5c9264113f11e1aeaeaf4fdc84b95
-size 6350
+oid sha256:9da5629fbee2dc87bdecb63e7bf95afbfad79636c59b512045ff133866ece52e
+size 6244
