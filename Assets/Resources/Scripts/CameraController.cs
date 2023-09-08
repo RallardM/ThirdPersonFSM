27523033775f2869d7d7a1965cf432866acae4f0
Remi Allard Mayer <remallmay@outlook.com>
2023-09-08 14:59:42 -0400

    corrected dynamic object interaction with camera

--- Assets/Resources/Scripts/CameraController.cs
+++ Assets/Resources/Scripts/CameraController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:32393e52f4cd4d8e6a586b23be8d1271dbbd4bd85dfaa2bd364930c0416a85f3
-size 6048
+oid sha256:947887b1cfa7fa72e6d39cb231ff44b000de4da9008ed447549a73896cf28901
+size 6340
