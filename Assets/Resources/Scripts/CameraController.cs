f30ff111363ee04e7b75a05aa34e1a22389a4dbe
Remi Allard Mayer <remallmay@outlook.com>
2023-09-09 17:02:49 -0400

    Corrected scrolling glitch

--- Assets/Resources/Scripts/CameraController.cs
+++ Assets/Resources/Scripts/CameraController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:33be30cc17f88e74273078f326e49834adc4afa5fd71c6b581ea0753cb261bf6
-size 8807
+oid sha256:8ce7cdfafb90c3024fb00cec45b19d672d0904f77aaa1bad814d10663e37a852
+size 6309
