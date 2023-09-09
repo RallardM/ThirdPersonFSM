2dff80d431913edfdb714ef56dbbac0d75750cb2
Remi Allard Mayer <remallmay@outlook.com>
2023-09-09 12:18:14 -0400

    Better scroll in/out field of view range

--- Assets/Resources/Scripts/CameraController.cs
+++ Assets/Resources/Scripts/CameraController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cc2e9bf556d78867e6a3e9925ebcb677afe1156e0f124d56347ba2cbb73bd359
-size 8260
+oid sha256:33be30cc17f88e74273078f326e49834adc4afa5fd71c6b581ea0753cb261bf6
+size 8807
