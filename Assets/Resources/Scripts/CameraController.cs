f35dab23fa91f53829885ee5b12bb2538b572ed3
Remi Allard Mayer <remallmay@outlook.com>
2023-09-09 17:19:29 -0400

    removed unused log

--- Assets/Resources/Scripts/CameraController.cs
+++ Assets/Resources/Scripts/CameraController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8ce7cdfafb90c3024fb00cec45b19d672d0904f77aaa1bad814d10663e37a852
-size 6309
+oid sha256:e288f951729df6be6a05cb59e3148a4c5b4e02a62fd69e98fd28caeee6a97c60
+size 6194
