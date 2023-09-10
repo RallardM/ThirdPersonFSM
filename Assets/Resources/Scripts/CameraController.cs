cb358a99719b77db5d40cb0c8dd37e292f353502
Remi Allard Mayer <remallmay@outlook.com>
2023-09-10 17:33:58 -0400

    raycast down for floor detection not working backup

--- Assets/Resources/Scripts/CameraController.cs
+++ Assets/Resources/Scripts/CameraController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5ea29c7dfa2d4b20c86da86d44da964556f4f89b6031ed4df7d7cec0a4f03425
-size 7475
+oid sha256:78eb94e6a2615b29649c97b40522a22586ceeac2028fbeac1939f78d379a5370
+size 8192
