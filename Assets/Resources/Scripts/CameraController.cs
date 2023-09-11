7d3c5033ad0a1a31c05b739e1e28877f86756833
Remi Allard Mayer <remallmay@outlook.com>
2023-09-11 12:39:46 -0400

    Working floor raycast

--- Assets/Resources/Scripts/CameraController.cs
+++ Assets/Resources/Scripts/CameraController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6cb4209e4bdf8bf3eedf0108477d30d9022d10f23934ee1d1ad8957c6e931d3a
-size 8201
+oid sha256:b371c5a309d607d7758fb7a7161755c2fd26ff0cb3a1c8ce07f11c8edbe11064
+size 8721
