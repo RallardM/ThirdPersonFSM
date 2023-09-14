3ef9419414dd6be71014d5a1055193b15813edf5
Rémi Allard-Mayer <rallardm@gmail.com>
2023-09-14 16:03:23 -0400

    added jump animator

--- Assets/Resources/Scripts/CharacterControllerStateMachine.cs
+++ Assets/Resources/Scripts/CharacterControllerStateMachine.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e22506f10033588947c89ad091fadadc7f8ed5bfc5851677a6268fddff292f3f
-size 2258
+oid sha256:a5326e3eb73b0046adc0160e184fa31dc04af3b8d45b0ad68cb22a8607dd56fd
+size 2598
