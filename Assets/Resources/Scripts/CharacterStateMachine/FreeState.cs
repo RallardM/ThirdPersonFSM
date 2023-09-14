91a5355631100717deaa7e3346fadf28bf5b32cf
Rémi Allard-Mayer <rallardm@gmail.com>
2023-09-14 16:03:23 -0400

    added jump animator

--- Assets/Resources/Scripts/CharacterStateMachine/FreeState.cs
+++ Assets/Resources/Scripts/CharacterStateMachine/FreeState.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3f033af6c4475ca288178f3948e24e3b7a2f901d81a40bab0a9b4967c893a4c4
-size 3666
+oid sha256:6fbcce407ec254c48fc1ddc32c57dc0c630b66540f04074495cdd04522570e3b
+size 3716
