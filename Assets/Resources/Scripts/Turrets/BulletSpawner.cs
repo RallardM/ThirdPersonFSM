e33ba83ed1f8d123a802ab503e1cf70403905d67
Rémi Allard-Mayer <rallardm@gmail.com>
2023-09-14 13:08:07 -0400

    Update to Unity 2022.3.7f1

--- Assets/Resources/Scripts/Turrets/BulletSpawner.cs
+++ Assets/Resources/Scripts/Turrets/BulletSpawner.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bc32cbc368432dd0b3a5a57d73d31dd64b1c4581ba3213f31673c2e5f97ea2e2
-size 1625
+oid sha256:6ba0db2eacb07dfbe2a2a03a7ca1b617cf360c343b7ea2bccf0dfcb98ddf8de3
+size 1665
