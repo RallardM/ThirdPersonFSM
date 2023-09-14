13420acffa7087e7cd4124fb91bbc23b6a231f72
Rémi Allard-Mayer <rallardm@gmail.com>
2023-09-14 15:13:25 -0400

    Animation integration backup

--- Assets/Resources/Scripts/CharacterControllerStateMachine.cs
+++ Assets/Resources/Scripts/CharacterControllerStateMachine.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0eb2a65a6dd5ee6a67a1d60e5b24b9311f0a05d9539891fb046fd7ead9d9e296
-size 2270
+oid sha256:e22506f10033588947c89ad091fadadc7f8ed5bfc5851677a6268fddff292f3f
+size 2258
