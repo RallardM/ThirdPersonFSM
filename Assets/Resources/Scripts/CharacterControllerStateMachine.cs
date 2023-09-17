dee951ee7225fdfcea4e055a9c048034f891a903
Remi Allard Mayer <remallmay@outlook.com>
2023-09-17 02:08:19 -0400

    lopping jump unstable

--- Assets/Resources/Scripts/CharacterControllerStateMachine.cs
+++ Assets/Resources/Scripts/CharacterControllerStateMachine.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:528f0303c6861fae10355789bbbc48c1eefbf070e5297ca1d0f79168191a66f4
-size 2940
+oid sha256:e9024fb0db279f5da007b3d34c9342b22dced1dc4bf7d9b5bc21647ff882252f
+size 3163
