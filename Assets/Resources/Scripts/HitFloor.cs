13ccc620bfe97d602be07be14656264bee6b38c8
Remi Allard Mayer <remallmay@outlook.com>
2023-09-17 16:52:31 -0400

    stable hit floor

--- Assets/Resources/Scripts/HitFloor.cs
+++ Assets/Resources/Scripts/HitFloor.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:741446188af1e42c205a1aa6f9a8c2bbd06c16a29d7da309b3db8356311cbaf6
-size 290
+oid sha256:25e4e333c5b667ff57b0a8e173acfb66a6aec6c754e6150233ef9080af57d9c1
+size 1714
