5dc77274442a7884e71acaad8fd4d80a02f67dc8
Remi Allard Mayer <remallmay@outlook.com>
2023-09-17 16:52:31 -0400

    stable hit floor

--- Assets/Resources/Scripts/CharacterStateMachine/GettingHitState.cs
+++ Assets/Resources/Scripts/CharacterStateMachine/GettingHitState.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e09786be69592378e18d1217c029d90694b534fd9a9088f739d8bd22b8695446
-size 571
+oid sha256:10e59e21e1da1745c561e811aeb48b4fd270b0f66a21c7baeb4e05a4eab14b01
+size 917
---
+++ Assets/Resources/Scripts/GameManager.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:9cbb62d3f8df4b07ccafa316ecbd8920372ea51a34e6005ad0c4310c1df65938
+size 510
