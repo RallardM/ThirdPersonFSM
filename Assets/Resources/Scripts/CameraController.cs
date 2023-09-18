337bb0789daceab63cccb1233d3accac0a06c82d
Remi Allard Mayer <remallmay@outlook.com>
2023-09-18 15:07:23 -0400

    hit raycast down working not completely

--- Assets/Resources/Scripts/CameraController.cs
+++ Assets/Resources/Scripts/CameraController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2fb75b9800c5feccdef1fa2180ca8b4eb1725715c3e25708da6a9c33909f7e70
-size 9871
+oid sha256:4138e6212db283cfbcbc82e8050d43e623fea8a8674ff4720e5c3e28ca7047bf
+size 10090
