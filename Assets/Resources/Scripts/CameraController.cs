4cd17502d6254c49d440226631b353ab9ec00fc7
Remi Allard Mayer <remallmay@outlook.com>
2023-09-18 13:32:35 -0400

    backup for second drastic change

--- Assets/Resources/Scripts/CameraController.cs
+++ Assets/Resources/Scripts/CameraController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:875719b611cbfa9c83dbd313d6174c2c2cb375ba943d514e59f9e77f5a72f9bf
-size 10198
+oid sha256:2fb75b9800c5feccdef1fa2180ca8b4eb1725715c3e25708da6a9c33909f7e70
+size 9871
