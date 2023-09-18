25fc4f322162f51895fad04dada82a200f4af0a9
Remi Allard Mayer <remallmay@outlook.com>
2023-09-18 11:57:01 -0400

    still not working backup to make a drastic change

--- Assets/Resources/Scripts/CameraController.cs
+++ Assets/Resources/Scripts/CameraController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b2605702bd1d7dd12cb0116a52dc1b533def4c0b7b568edac8986b1b1c45d51c
-size 9138
+oid sha256:b58d473776d23e97ace16c86cb5b195bf976713d8fe957b965889fa3bf2933b9
+size 9546
