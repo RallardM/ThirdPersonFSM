92d540b240f4cf5d3a5773a725c7e4bfe6b76774
Remi Allard Mayer <remallmay@outlook.com>
2023-09-20 06:45:54 -0400

    stable turrets

--- Assets/Resources/Scripts/Turrets/BulletSpawner.cs
+++ Assets/Resources/Scripts/Turrets/BulletSpawner.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6ba0db2eacb07dfbe2a2a03a7ca1b617cf360c343b7ea2bccf0dfcb98ddf8de3
-size 1665
+oid sha256:e5ad890b70a69ad7c780afdf78cd529f619e6811bee35882372500c11c422375
+size 3325
