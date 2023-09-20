2194b1c464d5961fa22d853ed76ff548a7776def
Remi Allard Mayer <remallmay@outlook.com>
2023-09-20 06:45:54 -0400

    stable turrets

--- Assets/Resources/Scripts/Turrets/BulletSpawner.cs
+++ Assets/Resources/Scripts/Turrets/BulletSpawner.cs
@@ -1,49 +1,89 @@
+// Source : https://unity.com/how-to/use-object-pooling-boost-performance-c-scripts-unity
 using UnityEngine.Pool;
 using UnityEngine;
+using System;
+using System.Collections;
 
 public class BulletSpawner : MonoBehaviour
 {
-    //[SerializeField] private GameObject projectilePrefab;
-    //[SerializeField] private float timeToReturnToPool = 10f;
-    //[SerializeField] private int maxPoolSize = 100;
+    [SerializeField] private GameObject m_projectilePrefab;
+    [SerializeField] private GameObject m_gunBarrel;
+    [SerializeField] private float m_timeToReturnToPool = 10.0f;
+    [SerializeField] private float m_projectileSpeed = 5.0f;
+    [SerializeField] private float m_fireRate = 1.0f;
+    [SerializeField] private int m_maxPoolSize = 100;
 
-    //private ObjectPool<GameObject> objectPool;
+    private ObjectPool<GameObject> m_projectilePool;
+    
 
-    //void Awake() => objectPool = new ObjectPool<GameObject>(CreateProjectile, OnGetProjectile, OnReleaseProjectile, null, true, maxPoolSize);
+    private void Awake()
+    {
+        m_projectilePool = new ObjectPool<GameObject>(
+            createFunc: () =>
+            {
+                GameObject projectile = Instantiate(m_projectilePrefab);
+                projectile.SetActive(false);
+                return projectile;
+            },
+            actionOnGet: (projectile) => projectile.SetActive(true),
+            actionOnRelease: (projectile) => projectile.SetActive(false),
+            actionOnDestroy: null,
+            collectionCheck: true,
+            defaultCapacity: m_maxPoolSize
+        );
+    }
 
-    //private void Awake()
-    //{
-    //    objectPool = new ObjectPool<GameObject>(
-    //        createFunc: () =>
-    //        {
-    //            GameObject projectile = Instantiate(projectilePrefab);
-    //            projectile.SetActive(false);
-    //            return projectile;
-    //        },
-    //        actionOnGet: (projectile) => projectile.SetActive(true),
-    //        actionOnRelease: (projectile) => projectile.SetActive(false),
-    //        actionOnDestroy: null,
-    //        collectionCheck: true,
-    //        defaultCapacity: maxPoolSize
-    //    );
-    //}
+    private void Start()
+    {
+        // Source : https://sd.blackball.lv/library/Unity_Games_by_Tutorials_3rd_Edition_(2018).pdf page 94
+        // Source : https://youtu.be/j-28BbzvgGk
+        // Repeat invoke makes the turret shoot automatically multiple bullets
+        InvokeRepeating(nameof(ShootProjectile), 0f, m_fireRate);
+    }
 
-    //private void Update()
-    //{
-    //    GameObject projectile = Instantiate(projectilePrefab, transform.position, transform.rotation);
-    //    projectile.transform.position = transform.position;
-    //    projectile.GetComponent<Rigidbody>().AddForce(transform.forward * 1000f);
-    //}
+    private void ShootProjectile()
+    {
+        GameObject projectile = m_projectilePool.Get();
+        if (projectile != null)
+        {
+            // Set the projectile's position and rotation
+            projectile.transform.position = m_gunBarrel.transform.position;
+            projectile.transform.rotation = m_gunBarrel.transform.rotation;
 
-    //public GameObject GetProjectile()
-    //{
-    //    GameObject projectile = objectPool.Get();
-    //    Invoke(nameof(ReturnToPool), timeToReturnToPool);
-    //    return projectile;
-    //}
+            // Get the Rigidbody component of the projectile
+            Rigidbody projectileRigidbody = projectile.GetComponent<Rigidbody>();
 
-    //private void ReturnToPool(GameObject projectile)
-    //{
-    //    objectPool.Release(projectile);
-    //}
+            // Set the velocity of the projectile in the forward direction of the gun barrel
+            projectile.transform.position = m_gunBarrel.transform.position;
+            projectile.transform.rotation = m_gunBarrel.transform.rotation;
+            projectileRigidbody.velocity = -m_gunBarrel.transform.up * m_projectileSpeed;
+
+            // Schedule the projectile to return to the pool after the specified delay
+            StartCoroutine(ReturnToPoolAfterDelay(projectile, m_timeToReturnToPool));
+        }
+    }
+
+    public GameObject GetProjectile()
+    {
+        GameObject projectile = m_projectilePool.Get();
+
+        // Shoot projectile from gun barrel in its forward direction
+        Vector3 forwardDirection = m_gunBarrel.transform.forward;
+        projectile.transform.position = m_gunBarrel.transform.position;
+        projectile.transform.rotation = Quaternion.LookRotation(forwardDirection);
+
+        return projectile;
+    }
+
+    private IEnumerator ReturnToPoolAfterDelay(GameObject projectile, float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        ReturnToPool(projectile);
+    }
+
+    private void ReturnToPool(GameObject projectile)
+    {
+        //Debug.Log("Return projectile to pool");
+        m_projectilePool.Release(projectile);
+    }
 }
