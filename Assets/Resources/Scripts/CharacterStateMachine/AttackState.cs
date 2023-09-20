455050d9e9a92e9511af81dd5abc1c29913728cd
Remi Allard Mayer <remallmay@outlook.com>
2023-09-20 03:25:07 -0400

    Stable attacking state

--- Assets/Resources/Scripts/CharacterStateMachine/AttackState.cs
+++ Assets/Resources/Scripts/CharacterStateMachine/AttackState.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:df9a7d70e2b0143ad809a7428e15f5714a8e0a2ef4259e5d63d932c07989831c
-size 817
+oid sha256:4f3ef37a8f41663576087edc7d8f5f9a4b3fee9ef56ed2f84f3502902e9357a9
+size 1439
--- Assets/Resources/Scripts/CharacterStateMachine/FreeState.cs
+++ Assets/Resources/Scripts/CharacterStateMachine/FreeState.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ebb7458e22975c709c3ad8a5f909e9408435e82099fcda50c573e8ee144e3731
-size 5238
+oid sha256:d1c32626c77f60db1744d43c9c671158438ef0cf9b028eeb4b535263cfdf7fd5
+size 5126
