a63e1629dc867a97e635a75ca570f9c6d4af378a
Remi Allard Mayer <remallmay@outlook.com>
2023-09-25 11:36:17 -0400

    backup before school working on falling damage

--- Assets/Resources/Scripts/CharacterStateMachine/FallingState.cs
+++ Assets/Resources/Scripts/CharacterStateMachine/FallingState.cs
@@ -1,7 +1,9 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class FallingState : CharacterState
 {
+    private float m_fallingvelocity = 0.0f;
     public override void OnEnter()
     {
         Debug.Log("Enter State: Falling state");
@@ -11,6 +13,7 @@ public class FallingState : CharacterState
     public override void OnExit()
     {
         Debug.Log("Exit state: Falling state");
+        if(Absolute(m_fallingvelocity) > 12.0f)
         m_stateMachine.UpdateAnimation(this);
     }
 
@@ -21,7 +24,8 @@ public class FallingState : CharacterState
 
     public override void OnUpdate()
     {
-
+        Debug.Log("Current falling velocity: " + m_stateMachine.RB.velocity.y);
+        m_fallingvelocity = m_stateMachine.RB.velocity.y;
     }
 
     public override bool CanEnter(CharacterState currentState)
