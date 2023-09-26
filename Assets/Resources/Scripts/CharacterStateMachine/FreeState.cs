bf6d33ec4d24caa85e2a9aaaa17dccd127a71f16
Remi Allard Mayer <remallmay@outlook.com>
2023-09-26 11:39:31 -0400

    last stable, backup for school, still trying to make the falling stunned animation, need to order the information to the animator so that it knows wether the player is stunned or is not after hitting the ground or other ways to trigger the stunned state.

--- Assets/Resources/Scripts/CharacterStateMachine/FreeState.cs
+++ Assets/Resources/Scripts/CharacterStateMachine/FreeState.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:07addea2d0bab4625a7023bba93116c85c6f6aa14a744f77fa0c70c744601a99
-size 5643
+oid sha256:670b593ba4e6c85ce3537550cec9aa195eac30d426278337cad4278c33f0f3fe
+size 5967
