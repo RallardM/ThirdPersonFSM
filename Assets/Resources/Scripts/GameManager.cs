f016f5d2bb7f833b7fb38d2732c2745ea9c63036
Remi Allard Mayer <remallmay@outlook.com>
2023-09-27 20:07:40 -0400

    stable camera offset obstruction

--- Assets/Resources/Scripts/GameManager.cs
+++ Assets/Resources/Scripts/GameManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0e92970dad2d0986dafc9adc6f85bac9a453a69ed72750bf422a27c90a2addc3
-size 543
+oid sha256:6e67db2007829f97e93eb7112ef80b44584e9f0fc8a316c4f5ab72b11e4d27bd
+size 545
