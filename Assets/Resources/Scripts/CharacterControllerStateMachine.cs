95edc0847cdd1fb6dd044de76dad43fdf55eecc4
Remi Allard Mayer <remallmay@outlook.com>
2023-09-28 12:00:38 -0400

    added getting hit in air state unstable

--- Assets/Resources/Scripts/CharacterControllerStateMachine.cs
+++ Assets/Resources/Scripts/CharacterControllerStateMachine.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f8de5b28c8e47e760b073acd975952ab60b67f48da0ab3ba736f0375910b52cd
-size 8936
+oid sha256:151b3b8d4c692b2431e7298b2ad7b29e6261c4d050e0e617e59f2479a290000e
+size 9065
--- Assets/Resources/Scripts/CharacterStateMachine/GettingHitState.cs
+++ Assets/Resources/Scripts/CharacterStateMachine/GettingHitState.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b89a2fadfa41fee979f7c29e49257c5194aa82b6e558a63d16adc0568b643943
-size 945
+oid sha256:df0beaefaab8315983402974ce6f5316ac190439a015a3f541fdfb826bea2111
+size 1026
