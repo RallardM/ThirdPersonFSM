54376fba9a0dea8909f7d922d96c04f550373093
Remi Allard Mayer <remallmay@outlook.com>
2023-09-27 23:39:37 -0400

    fist commit

--- Assets/Resources/Scripts/CameraController.cs
+++ Assets/Resources/Scripts/CameraController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c37baeea08c05190d2e031225b2493a8ca15956b3327ca56ee134f4a5cc704cb
-size 13182
+oid sha256:8eb65019cabf052d0d52b209b47ef35f199d038fa1c1b4934b016fb58f7def48
+size 13845
