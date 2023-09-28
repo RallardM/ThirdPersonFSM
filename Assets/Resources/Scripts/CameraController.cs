8ce6dbff4e52dd55e7cc1a5a468c11a6a631afbe
Rémi Allard-Mayer <rallardm@gmail.com>
2023-09-28 15:14:52 -0400

    Camera obstruct getting somwhere backup

--- Assets/Resources/Scripts/CameraController.cs
+++ Assets/Resources/Scripts/CameraController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3436e60f56b09c9aeb2075e45c472f46dda279df5df079397f642394d9fd3cf6
-size 10183
+oid sha256:7fc40bf3c5f5ec01784cff2c80899b662a9399df0cea34e3f2b8e4c5fc6a6f67
+size 13119
