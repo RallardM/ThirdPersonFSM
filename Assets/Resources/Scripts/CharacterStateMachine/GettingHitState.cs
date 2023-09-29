91c5db0de089642ae39c927bc706545de46bd94d
Remi Allard Mayer <remallmay@outlook.com>
2023-09-28 23:14:37 -0400

    corrected sandbox assets

--- Assets/Resources/Scripts/CharacterStateMachine/GettingHitState.cs
+++ Assets/Resources/Scripts/CharacterStateMachine/GettingHitState.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:df0beaefaab8315983402974ce6f5316ac190439a015a3f541fdfb826bea2111
-size 1026
+oid sha256:a10760c9564658d43dee7a8eb252580230f0df2adf4a882a68066e6d343f73cf
+size 1405
