1859c8e63a665ad600127cbbffd7755fcf97e2d8
Remi Allard Mayer <remallmay@outlook.com>
2023-09-28 21:37:28 -0400

    camera obstruction is better but still jitters a bit and the position losck the mouse

--- Assets/Resources/Scripts/CameraController.cs
+++ Assets/Resources/Scripts/CameraController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7a82ce1805b74a91785a6e6409f52e712524a0a9214ca1af68ff49d01bd89cb3
-size 12778
+oid sha256:a501784cf4aff4728d2509b5420c2312986832e56e7aae2aed77caa67d9b736d
+size 12752
