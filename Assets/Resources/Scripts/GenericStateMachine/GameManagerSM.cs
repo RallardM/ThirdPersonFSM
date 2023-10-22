8c43897051f38bb55ca122bce75c97e474d7e8c8
Remi Allard Mayer <remallmay@outlook.com>
2023-10-21 23:31:57 -0400

    added SceneTransitionState

--- Assets/Resources/Scripts/GenericStateMachine/GameManagerSM.cs
+++ Assets/Resources/Scripts/GenericStateMachine/GameManagerSM.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:20b703662b808ba4246e5ead6539b39f9ecacac66777d17b2f3750ef6f2a6491
-size 1414
+oid sha256:fa1f820e730ea08a1018dfc509a2402851088e81e652e33c30f7f8ba4aef116b
+size 1530
