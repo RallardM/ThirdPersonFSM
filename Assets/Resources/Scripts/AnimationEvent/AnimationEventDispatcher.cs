6b23b62f6b12d2a75d234497fc9009df6607efd3
Remi Allard Mayer <remallmay@outlook.com>
2023-10-22 01:37:44 -0400

    Adding event in the attack animation Adding AnimationEventDispatcher to controll the attack animation events

---
+++ Assets/Resources/Scripts/AnimationEvent/AnimationEventDispatcher.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:f0b0696a8899433ff1b9b831c03d6a4dd4a5f940fef4d9f8b8b4fc031b2bf42a
+size 442
--- Assets/Resources/Scripts/HitTestObject.cs
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:7cc716961b17c79b7fe6c955a8db261ce133c0fd0f5f6d2d51270cf7dea058c9
-size 2285
