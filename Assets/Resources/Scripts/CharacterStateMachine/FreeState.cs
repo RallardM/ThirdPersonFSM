8d348d702d622587ff5011c770ebc8b4355afddb
Remi Allard Mayer <remallmay@outlook.com>
2023-10-21 23:15:16 -0400

    The player fully stops when changing the camera state to cinema state

--- Assets/Resources/Scripts/CharacterStateMachine/FreeState.cs
+++ Assets/Resources/Scripts/CharacterStateMachine/FreeState.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:47574ff473c500d76d23dda1fc1a603c58e52d9c4ac7ce8b701cde1b733efe7b
-size 6500
+oid sha256:e9b48b9bab316627c1035b03f13510f04e726e01e24448418097aa3c59780b16
+size 6593
