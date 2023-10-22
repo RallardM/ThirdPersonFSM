83f3f6d5ac39c118eb4cf3792fdb4452d2b116c4
Remi Allard Mayer <remallmay@outlook.com>
2023-10-21 22:46:10 -0400

    Renamed CameraController class to MainCameraController to differentiate its function that are solely focused on the main camera rather than on the CineCamera

--- Assets/Resources/Scripts/CameraController.cs
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:f870b3e8f29a2bf7e0a1f22927c492e65f9351f4f413b8a902ebb516b342be7d
-size 10836
---
+++ Assets/Resources/Scripts/MainCameraController.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:448bf803511295e86cf7adcb7c70c6bd28f4f7102cd105e5ce9a7597d4851eec
+size 10840
