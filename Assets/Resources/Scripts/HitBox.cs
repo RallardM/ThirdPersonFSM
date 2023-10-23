5d9dc27433dfdc38735d7057feccd817ee94333d
Remi Allard Mayer <remallmay@outlook.com>
2023-10-22 23:24:52 -0400

    Slap effect working

--- Assets/Resources/Scripts/HitBox.cs
+++ Assets/Resources/Scripts/HitBox.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:54731e97b0aa1dd49fdee1fd103ba424a0ebdf20c8eb238141958f1c86d3ad71
-size 1157
+oid sha256:67b7ac404b54d5f8ae2027e031b2a396407a4ff91f25631919c40f28723486dc
+size 1254
