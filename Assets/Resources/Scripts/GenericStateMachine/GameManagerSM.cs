1f91ef74edb75638b6323fe0fde9dc1ee43613b1
Remi Allard Mayer <remallmay@outlook.com>
2023-10-24 06:27:35 -0400

    Adding a pause music

--- Assets/Resources/Scripts/GenericStateMachine/GameManagerSM.cs
+++ Assets/Resources/Scripts/GenericStateMachine/GameManagerSM.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:95a5a879bbe3465a54fdc944c932ec5ab611da35ce2b9eeb11c77b11ae9bf44f
-size 1705
+oid sha256:faeea4ebb305be63cffcf65ff9c6ef93d300bad308fbbf665c8fb702c7da7e0c
+size 2017
