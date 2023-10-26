d88cee0472ba42a79320994396ae92d5e242b7ed
Rémi Allard-Mayer <rallardm@gmail.com>
2023-10-26 15:51:10 -0400

    Enemy slap is working

---
+++ Assets/Resources/Scripts/Enemy/EnemyAnimationEventDispatcher.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:cfa59f17ccbf9343dd2c66c9b0e43b849a45835a35d555eb78effdd3bb8bfa47
+size 493
---
+++ Assets/Resources/Scripts/Enemy/EnemyController.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:df461a4ee9e02b5a927578ed9c33ca657047f92d55d50f40874c90d5cf5e8ae3
+size 404
