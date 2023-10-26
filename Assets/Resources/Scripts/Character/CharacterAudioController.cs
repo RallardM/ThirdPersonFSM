141962e87b4a2b336bc46c623aca01e2538ad29e
Rémi Allard-Mayer <rallardm@gmail.com>
2023-10-26 16:14:29 -0400

    Adding all the slap effects

--- Assets/Resources/Scripts/Character/CharacterAudioController.cs
+++ Assets/Resources/Scripts/Character/CharacterAudioController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1dcead43c845150558cc0b315e0f46fe68228f665918a0a8260dff0b6cb40392
-size 2219
+oid sha256:906e9c3278144bfa98c049232d8a6a73873d28ed685ad3b235cfb1a71cd0d1c7
+size 2244
--- Assets/Resources/Scripts/Managers/VFXManager.cs
+++ Assets/Resources/Scripts/Managers/VFXManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d2843d2673fa79472f87b07b3528c858b7aac38b600654bd2291691610137ab9
-size 1032
+oid sha256:d95f46a6d4b7312f0d1edf31ca9d04875aafda1cce20d091b67449ba3cfcc9c5
+size 1295
