772d41191e07956c3b7d5f61ae033e730fec5a8a
Remi Allard Mayer <remallmay@outlook.com>
2023-10-26 00:04:47 -0400

    Slaps stable

--- Assets/Resources/Scripts/HitBox.cs
+++ Assets/Resources/Scripts/HitBox.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:67b7ac404b54d5f8ae2027e031b2a396407a4ff91f25631919c40f28723486dc
-size 1254
+oid sha256:7b1c482df5651a8c6e22f5ba4184a4341c84b7b37fca97b8ab018c70b64c1e61
+size 1393
