831d3929a0f6d15525202623ea263246cf8c3b8f
Remi Allard Mayer <remallmay@outlook.com>
2023-10-27 17:52:44 -0400

    camera shake from slap stable

---
+++ Assets/Resources/Scripts/Camera/CameraShake.cs
@@ -0,0 +1,55 @@
+// Source : https://www.youtube.com/watch?v=luSVUPU6bK0
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+using System.Threading;
+
+public class CameraShake : MonoBehaviour
+{
+    private CinemachineVirtualCamera m_virtualCamera;
+    private float m_shakeIntensity = 6.0f;
+    private float m_shakeTime = 0.2f;
+    private float m_timer;
+    private CinemachineBasicMultiChannelPerlin m_noise;
+
+    private void Awake()
+    {
+        m_virtualCamera = GetComponent<CinemachineVirtualCamera>();
+        m_noise = m_virtualCamera.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
+    }
+
+    private void Start()
+    {
+        StopShake();
+    }
+
+    public void ShakeCamera(float shakeIntensity)
+    {
+        m_noise.m_AmplitudeGain = shakeIntensity;
+        m_timer = m_shakeTime;
+    }
+
+    void StopShake()
+    {
+        m_noise.m_AmplitudeGain = 0.0f;
+        m_timer = 0.0f;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            ShakeCamera(m_shakeIntensity);
+        }
+
+        if (m_timer > 0.0f)
+        {
+            m_timer -= Time.deltaTime;
+            if (m_timer <= 0.0f)
+            {
+                StopShake();
+            }
+        }
+    }
+}
--- Assets/Resources/Scripts/HitBox.cs
+++ Assets/Resources/Scripts/HitBox.cs
@@ -1,10 +1,15 @@
+using Cinemachine;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class HitBox : MonoBehaviour
 {
     [SerializeField]
     private CharacterAudioController m_audioController;
+
+    [SerializeField]
+    private CameraShake m_cameraShake;
 
     [SerializeField]
     protected bool m_canHit;
@@ -17,6 +22,46 @@ public class HitBox : MonoBehaviour
 
     [SerializeField]
     protected List<EAgentType> m_affectedAgentTypes = new List<EAgentType>();
+
+    private Vector3 m_previousPosition;
+    private Rigidbody m_hitboxRigidBody;
+    public Vector3 m_globalVelocity;
+
+    void Start()
+    {
+        if (GetComponent<Rigidbody>() == null)
+        {
+            return;
+        }
+
+        m_hitboxRigidBody = GetComponent<Rigidbody>();
+        m_previousPosition = m_hitboxRigidBody.transform.position;
+    }
+
+    void Update()
+    {
+        if (m_hitboxRigidBody == null)
+        {
+            return;
+        }
+
+        //if (Input.GetKeyDown(KeyCode.I))
+        //{
+        //    if (m_impulseSource == null)
+        //    {
+        //        return;
+        //    }
+
+        //    var intensity = Random.Range(1.0f, 4.0f);
+        //    m_impulseSource.GenerateImpulse(intensity);
+        //}
+
+        // Since the rigidbody is on the squeletton, we need to compute the velocity ourselves
+        Vector3 currentPosition = m_hitboxRigidBody.transform.position;
+        Vector3 velocity = (currentPosition - m_previousPosition) / Time.deltaTime;
+        m_previousPosition = currentPosition;
+        m_globalVelocity = m_hitboxRigidBody.transform.InverseTransformDirection(velocity);
+    }
 
     protected void OnTriggerEnter(Collider collider)
     {
@@ -43,7 +88,16 @@ public class HitBox : MonoBehaviour
 
     protected void GetHit(HitBox otherHitBox)
     {
-        Debug.Log(gameObject.name + " got hit by " + otherHitBox.gameObject.name);
+        //Debug.Log(gameObject.name + " got hit by " + otherHitBox.gameObject.GetComponentInParent<EnemyController>().name);
+        //Debug.Log(gameObject.name + " got hit by hit box " + otherHitBox.gameObject.name);
+        //Debug.Log("Velocity : " + otherHitBox.gameObject.GetComponent<Rigidbody>().velocity);
+        Rigidbody rb = otherHitBox.gameObject.GetComponent<Rigidbody>();
+        Vector3 localVelocity = rb.velocity;
+        Vector3 globalVelocity = rb.transform.TransformDirection(localVelocity);
+        //Debug.Log("Global Velocity : " + globalVelocity);
+        //Debug.Log("G Velocity : " + otherHitBox.m_globalVelocity);
+        //m_impulseSource.GenerateImpulse(otherHitBox.m_globalVelocity);
+        m_cameraShake.ShakeCamera(otherHitBox.m_globalVelocity.magnitude);
     }
 }
 
