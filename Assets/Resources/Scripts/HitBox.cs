370806750f75517d88df6711ac1dfcffea63e7b9
Remi Allard Mayer <remallmay@outlook.com>
2023-10-27 17:55:55 -0400

    comment out old hitbox

--- Assets/Resources/Scripts/HitBox.cs
+++ Assets/Resources/Scripts/HitBox.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:384d22c689b387be59e689ff1d0f494c0f057141f13386c9c91057ccc7f13f5c
-size 3427
+oid sha256:751035b90def659ef5ded1a3ec5ced1634d16be88930a5f6e60d95885e29705f
+size 3433
