7ee1536cd6c3aa5d05e98535133b326a04a6346a
Remi Allard Mayer <remallmay@outlook.com>
2023-10-28 20:06:25 -0400

    Emote stable

--- Assets/Resources/Scripts/GenericStateMachine/CinematicState.cs
+++ Assets/Resources/Scripts/GenericStateMachine/CinematicState.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b62d3fecca13421b75a9af1ca02ef29f1c056288b2d8b527de31415c1da1783c
-size 1479
+oid sha256:71dd58bfbe9b99e88a8fa8bf7de71ef315c97f98a3d614273b9da911933da3a6
+size 1438
--- Assets/Resources/Scripts/GenericStateMachine/GameplayState.cs
+++ Assets/Resources/Scripts/GenericStateMachine/GameplayState.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:12fa7359c9c9d41a00abddd4881c6d34ea95aa6c7832c0c2be5cb1697c419bcd
-size 1337
+oid sha256:73c7e6ba4f8a58ea5533daf19e19a787a7b95767fed77245d9564af6bcf4525d
+size 1368
